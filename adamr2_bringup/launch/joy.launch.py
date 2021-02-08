261cfcf21fb6843d689c035bd45e6f6fcedb6eaf
fjnkt98 <50233866+fjnkt98@users.noreply.github.com>
2021-02-08 16:00:42 +0900

    Update refine launch syntax

--- adamr2_bringup/launch/joy.launch.py
+++ adamr2_bringup/launch/joy.launch.py
@@ -3,30 +3,50 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 import launch
-import launch_ros.actions
+import launch_ros
+
 
 def generate_launch_description():
-    device_port = launch.substitutions.LaunchConfiguration('device_port')
+    descriptions = launch.LaunchDescription()
+
+    device_path = launch.substitutions.LaunchConfiguration('device_path')
     config_path = launch.substitutions.LaunchConfiguration('config_path')
 
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument('device_port', default_value='/dev/input/js0'),
-        launch.actions.DeclareLaunchArgument('config_path', default_value=[launch.substitutions.TextSubstitution(text=os.path.join(get_package_share_directory('adamr2_bringup'), 'config', 'joy.config.yml'))]),
+    declare_device_path = launch.actions.DeclareLaunchArgument(
+            'device_path',
+            default_value='/dev/input/js0'
+            )
 
-        launch_ros.actions.Node(
+    declare_config_path = launch.actions.DeclareLaunchArgument(
+            'config_path',
+            default_value=os.path.join(
+                get_package_share_directory('adamr2_bringup'),
+                'config',
+                'joy.config.yaml'
+                )
+            )
+
+    joy_node = launch_ros.actions.Node(
             package='joy',
-            node_executable='joy_node',
+            executable='joy_node',
             name='joy_node',
             parameters=[{
-                'dev': device_port,
+                'dev': device_path,
                 'deadzone': 0.3,
                 'autorepeat_rate': 20.0
-            }]
-        ),
-        launch_ros.actions.Node(
+                }]
+            )
+
+    teleop_twist_joy_node = launch_ros.actions.Node(
             package='teleop_twist_joy',
-            node_executable='teleop_node',
+            executable='teleop_node',
             name='teleop_twist_joy_node',
             parameters=[config_path]
-        )
-    ])
+            )
+
+    descriptions.add_action(declare_config_path)
+    descriptions.add_action(declare_device_path)
+    descriptions.add_action(joy_node)
+    descriptions.add_action(teleop_twist_joy_node)
+
+    return descriptions
--- adamr2_bringup/launch/ypspur_ros.launch.py
+++ adamr2_bringup/launch/ypspur_ros.launch.py
@@ -3,30 +3,64 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 import launch
-import launch_ros.actions
+import launch_ros
+
 
 def generate_launch_description():
-    config_filepath = launch.substitutions.LaunchConfiguration('config_filepath')
-    param_filepath = launch.substitutions.LaunchConfiguration('param_filepath')
+    descriptions = launch.LaunchDescription()
 
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument('config_filepath',
-                            default_value=os.path.join(
-                                get_package_share_directory('adamr2_bringup'),
-                                'config',
-                                'ypspur_ros.config.yml')
-                            ),
-        launch.actions.DeclareLaunchArgument('param_filepath',
-                            default_value=os.path.join(
-                                get_package_share_directory('adamr2_bringup'),
-                                'config',
-                                'adamr2.param')
-                            ),
-        launch_ros.actions.Node(
-            package='ypspur_ros',
-            node_executable='ypspur_ros',
-            name='ypspur_ros',
-            parameters=[config_filepath, {"param_file": param_filepath}],
+    config_filepath = launch.substitutions.LaunchConfiguration(
+            'config_filepath')
+    param_filepath = launch.substitutions.LaunchConfiguration('param_filepath')
+    device_path = launch.substitutions.LaunchConfiguration('device_path')
+
+    declare_config_filepath = launch.actions.DeclareLaunchArgument(
+            'config_filepath',
+            default_value=os.path.join(
+                get_package_share_directory('adamr2_bringup'),
+                'config',
+                'ypspur_ros.config.yaml'
+                )
+            )
+
+    declare_param_filepath = launch.actions.DeclareLaunchArgument(
+            'param_filepath',
+            default_value=os.path.join(
+                get_package_share_directory('adamr2_bringup'),
+                'config',
+                'adamr2.param'
+                )
+            )
+
+    declare_device_path = launch.actions.DeclareLaunchArgument(
+            'device_path',
+            default_value='/dev/ttyACM0'
+            )
+
+    ypspur_coordinator = launch.actions.ExecuteProcess(
+            cmd=[
+                'ypspur-coordinator',
+                '-p',
+                param_filepath,
+                '-d',
+                device_path,
+                '--without-device',
+                '--without-control'
+                ],
             output='screen'
-        )
-    ])
+            )
+    ypspur_ros_node = launch_ros.actions.Node(
+            package='ypspur_ros2',
+            executable='ypspur_ros2_node',
+            name='ypspur_ros2_node',
+            parameters=[config_filepath],
+            output='screen'
+            )
+
+    descriptions.add_action(declare_config_filepath)
+    descriptions.add_action(declare_param_filepath)
+    descriptions.add_action(declare_device_path)
+    descriptions.add_action(ypspur_coordinator)
+    descriptions.add_action(ypspur_ros_node)
+
+    return descriptions
